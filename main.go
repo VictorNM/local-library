79a9bdd66f222e011861b571fbf3d9f5a65b60fc
victornm <victor.nguyenmau@gmail.com>
2019-01-28 20:22:47 +0700

    create catalog handler

--- main.go
+++ main.go
@@ -6,12 +6,47 @@ import (
 	"net/http"
 )
 
-func sayHello(w http.ResponseWriter, r *http.Request) {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello")
 }
 
+func catalogHandler(w http.ResponseWriter, r *http.Request) {
+	catalog := r.URL.Path[len("/catalog/"):]
+	fmt.Println(catalog)
+	switch catalog {
+	case "book":
+		bookHandler(w, r)
+	case "author":
+		authorHandler(w, r)
+	case "genre":
+		genreHandler(w, r)
+	case "bookinstance":
+		bookInstanceHandler(w, r)
+	default:
+		http.Error(w, "NOT FOUND", http.StatusNotFound)
+	}
+}
+
+func bookHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "NOT IMPLEMENTED")
+}
+
+func authorHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "NOT IMPLEMENTED")
+}
+
+func genreHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "NOT IMPLEMENTED")
+}
+
+func bookInstanceHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "NOT IMPLEMENT")
+}
+
 func main() {
-	http.HandleFunc("/", sayHello)
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/catalog/", catalogHandler)
+
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err.Error())
---
+++ web/app/controller/author.go
@@ -0,0 +1,10 @@
+package controller
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func authorListHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "NOT IMPLEMENTED")
+}
